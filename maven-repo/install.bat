054e8205ec1303ddda5be55763ce9bf8bf86a21e
Sumeet Kulkarni <sumeetk.n42@gmail.com>
2014-11-18 11:31:37 +0530

    Added adapatable jar to repo.

--- maven-repo/install.bat
+++ maven-repo/install.bat
@@ -1,1 +1,2 @@
 call mvn install:install-file -Dfile=./lib/kairosdb-client-1.0-SNAPSHOT.jar -DgroupId=kairosdb-client -DartifactId=kairosdb-client -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=./repository
+call mvn install:install-file -Dfile=./lib/n42-adaptable-1.0-SNAPSHOT.jar -DgroupId=com.n42 -DartifactId=n42-adaptable -Dversion=1.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=./repository
